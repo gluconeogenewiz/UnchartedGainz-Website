171aa019c7cff5d2d0dfdecbf4de1974fc9444fc
gluconeogenewiz <aqnguyen@ucsd.edu>
2023-11-07 09:39:50 -0800

    first commit

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Workouts!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://www.dmoose.com/cdn/shop/articles/Main-Image_f389f0f9-ef75-42ec-98bf-0f0259ad540a.jpg?v=1666358433'
+              text='Explore the killer leg workouts'
+              label='Legs'
+              path='/workouts'
+            />
+            <CardItem
+              src='https://www.dmoose.com/cdn/shop/articles/feature-image_664d327f-547e-4e9e-aae3-3e9d651d2cea.jpg?v=1683545606'
+              text='Tap into muscle numbing chest excercises'
+              label='Chest'
+              path='/workouts'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://www.dmoose.com/cdn/shop/articles/1_2_1.jpg?v=1656429275'
+              text='Set Sail in the Uncharted Waters of Maximum Bicep Gainz'
+              label='Biceps'
+              path='/workouts'
+            />
+            <CardItem
+              src='https://www.dmoose.com/cdn/shop/articles/Main_Image_85cf48c3-959d-42d4-a91a-c54959994076.jpg?v=1688809842'
+              text='Gain flawless tricep through these tricep workouts'
+              label='Triceps'
+              path='/workouts'
+            />
+            <CardItem
+              src='https://www.t-nation.com/wp-content/uploads/2019/12/11-Ab-Exercises-Youll-Love-to-Hate.jpeg'
+              text='Killer Core workouts'
+              label='Core'
+              path='/workouts'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/pages/Workouts.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Workouts() {
+  return <h1 className='workouts'>WORKOUTS</h1>;
+}
